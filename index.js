424d88a8bea8953a8c0243f5052bdf5de788f149
wendy <mizukilee@gmail.com>
2022-11-11 10:22:31 -0500

    commit tree 8f6e37f82979d8fb5af4306b8a4c0a882ab5e669

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ['Milo','Otis', 'Garfield']
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name) { 
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(name) { 
+    cats.pop(name)
+}
+
+function destructivelyRemoveFirstCat(name) { 
+    cats.shift(name)
+}
+
+function appendCat(name) {
+    const allCats = [...cats,name]
+    return(allCats)
+}
+
+function prependCat(name) { 
+    const moreCats = [name,...cats]
+    return(moreCats)
+}
+
+function removeLastCat() { 
+    return(cats.slice(0,-1))
+}
+
+function removeFirstCat() {
+    return(cats.slice(1))
+}
